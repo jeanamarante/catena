77b898edbc54af2b54c16466608303af809be6e0
Jean Amarante <jeanamaranteopensource@gmail.com>
2018-09-03 09:33:18 -0400

    {Update: dependencies.js} Module restructure.

--- tasks/lib/compile/dependencies.js
+++ tasks/lib/compile/dependencies.js
@@ -1,3 +1,97 @@
-module.exports = function (grunt, files) {
+var path = require('path');
+var modules = [];
+var hierarchy = {};
 
+function createHierarchyNode (parentName, childName) {
+    if (hierarchy[childName] !== undefined) { return undefined; }
+
+    hierarchy[childName] = {
+        parent: parentName,
+        absPath: '',
+        children: []
+    };
+}
+
+function parseFile (grunt, absPath) {
+    var content = grunt.file.read(absPath);
+
+    if (parseClassExtend(content, absPath)) {
+        return undefined;
+    } else if (parseClass(content, absPath)) {
+        return undefined;
+    } else {
+        modules.push(absPath);
+    }
+}
+
+function parseClassExtend (content, absPath) {
+    var result = /^\s*extend\s*\(\s*['"](.*)['"]\s*,\s*['"](.*)['"]\s*\)/.exec(content);
+
+    if (result === null) { return false; }
+
+    var parentName = result[1];
+    var childName = result[2];
+
+    createHierarchyNode('', parentName);
+    createHierarchyNode(parentName, childName);
+
+    hierarchy[parentName].children.push(childName);
+
+    hierarchy[childName].parent = parentName;
+    hierarchy[childName].absPath = absPath;
+
+    return true;
+}
+
+function parseClass (content, absPath) {
+    var result = /^\s*CLASS\s*\.\s*([A-Za-z0-9-_]+)/.exec(content);
+
+    if (result === null) { return false; }
+
+    var name = result[1];
+
+    createHierarchyNode('', name);
+
+    hierarchy[name].absPath = absPath;
+
+    return true;
+}
+
+function convertHierarchyToArray () {
+    var arr = [];
+    var classNames = Object.keys(hierarchy);
+
+    for (var i = 0, max = classNames.length; i < max; i++) {
+        var name = classNames[i];
+
+        if (hierarchy[name].parent === '') {
+            arr.push(hierarchy[name].absPath);
+
+            appendChildrenToArray(name, arr);
+        }
+    }
+
+    arr = arr.concat(modules);
+
+    return arr;
+}
+
+function appendChildrenToArray (parentName, arr) {
+    var children = hierarchy[parentName].children;
+
+    for (var i = 0, max = children.length; i < max; i++) {
+        var childName = children[i];
+
+        arr.push(hierarchy[childName].absPath);
+
+        appendChildrenToArray(childName, arr);
+    }
+}
+
+module.exports = function (grunt, files) {
+    for (var i = 0, max = files.length; i < max; i++) {
+        parseFile(grunt, files[i]);
+    }
+
+    return convertHierarchyToArray();
 };
